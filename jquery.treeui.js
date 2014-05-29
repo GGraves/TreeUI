9e3359cc4125b0b52e488605b0f32eed04dc2a0b
Gavin <Gavin@JGG.email>
2014-05-28 17:05:54 -0700

    Fixed .js formatting

--- jquery.treeui.js
+++ jquery.treeui.js
@@ -1,16 +1,17 @@
-(function($)
+( function( $)
 { 
-    $(document).ready(function(){
-        $(".body").css({"display":"none"}); 
-        $(".title").click(function(){
-            $(this).siblings().slideToggle("slow");
-            $(this).parent().siblings().slideToggle("slow");
-
-            if( !$(this).parent().parent().is(".tree"))
+    $( document).ready( function()
+    {
+        $( ".body").css( { "display":"none"}); 
+        $( ".title").click( function()
+        {
+            $( this).siblings().slideToggle( "slow");
+            $( this).parent().siblings().slideToggle( "slow");
+            if( !$( this).parent().parent().is( ".tree"))
             {
-                $(this).parent().parent().siblings().slideToggle("slow");
+                $( this).parent().parent().siblings().slideToggle( "slow");
             }
         });
     });
 }
-(jQuery));
+( jQuery));
