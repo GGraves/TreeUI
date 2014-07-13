6ee925f7d3b89003c9a25f28991fadc0f2e3faac
Gavin <Gavin@JGG.email>
2014-07-13 00:18:28 -0700

    updated for smoother transitions and title focusing

--- jquery.treeui.js
+++ jquery.treeui.js
@@ -2,15 +2,21 @@
 { 
     $( document).ready( function()
     {
-        $( ".body").css( { "display":"none"}); 
+        $( ".body").css( { "display":"none"});
+        $( ".branch-break").css( { "display":"none"});
         $( ".title").click( function()
         {
-            $( this).siblings().slideToggle( "slow");
-            $( this).parent(".branch").siblings().slideToggle( "slow");
             if( !$( this).parent().parent().is( ".tree"))
             {
                 $( this).parent().parent().siblings().slideToggle( "slow");
             }
+            $( this).parent().siblings().slideToggle( "slow");
+              
+            $( this).siblings().slideToggle( "slow", function()
+            {
+                $('body').animate({scrollTop: $(".tree").offset().top}); 
+            });
+            $( this).children(".branch-break").slideToggle( "slow");
         });
     });
 }
